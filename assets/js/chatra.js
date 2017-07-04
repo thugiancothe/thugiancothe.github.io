97be7d59b1e2fa9a95e3f90813d804ba8b22f6d4
Cafe4it <cafe4it@gmail.com>
2017-07-04 16:08:26 +0700

    Deploy to GitHub Pages

---
+++ assets/js/chatra.js
@@ -0,0 +1,13 @@
+/* global ChatraID*/
+ChatraID = 'cP2twfSmZ6mF6osK6';
+    (function(d, w, c) {
+        var n = d.getElementsByTagName('script')[0],
+            s = d.createElement('script');
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+            + '//call.chatra.io/chatra.js';
+        n.parentNode.insertBefore(s, n);
+    })(document, window, 'Chatra');
---
+++ assets/js/zatobox.js
@@ -0,0 +1,1 @@
+(function(d,s,id){var z=d.createElement(s);z.type="text/javascript";z.id=id;z.async=true;z.src="//static.zotabox.com/9/5/95a4945a8707687138108cd382296c90/widgets.js";var sz=d.getElementsByTagName(s)[0];sz.parentNode.insertBefore(z,sz)}(document,"script","zb-embed-code"));
